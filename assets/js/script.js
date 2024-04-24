84a4e62b3e447e33d88fe8966278cd985fda345a
sanskardrolia <sanskardrolia@gmail.com>
2024-04-24 20:34:48 +0530

    Fixed NavBar - Bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,9 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+// // add click event to modal close button
+// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+// overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
